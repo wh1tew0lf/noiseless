0bda9ac5b81ffa656e5e3a1e26d9b3557eea0d4c
Volkov Danil <volkovdanil91@gmail.com>
2018-03-16 14:16:31 +0600

    Add test for BCHCoder

--- src/test/java/my/thesis/MainTest.java
+++ src/test/java/my/thesis/MainTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class MainTest {
 
     @Test
-    public void findByNameTest() {
+    public void exampleTest() {
         assertEquals(true, true);
     }
 }
---
+++ src/test/java/my/thesis/noiseless/bch/BCHCoderTest.java
@@ -0,0 +1,72 @@
+package my.thesis.noiseless.bch;
+
+import org.junit.Test;
+
+import java.util.Random;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class BCHCoderTest {
+
+    @Test
+    public void simpleTest() {
+        int dataLenMax = 4094;
+        int blockSize = 255;
+        Random rnd = new Random();
+        for (int i = 0; i < 100; ++i) {
+            System.out.println(i);
+            System.out.println(1 + (rnd.nextInt(50) % dataLenMax));
+            byte[] data1 = new byte[1 + (rnd.nextInt(50) % dataLenMax)];
+            for (int j = 0; j < data1.length; ++j) {
+                data1[j] = (byte) (rnd.nextInt(50) % 256);
+            }
+
+            int t = (0 == blockSize)
+                    ? (1 + rnd.nextInt(50) % ((int) Math.round(data1.length * 0.17)))
+                    : (1 + rnd.nextInt(50) % ((int) Math.round(blockSize * 0.17)));
+
+            byte[] data2 = null;
+            try {
+                data2 = BCHCoder.encode_systematic(data1, t, blockSize);
+            } catch (BchException e) {
+                fail(e.getMessage());
+            }
+            assertNotNull(data2);
+            int errCnt = 0;
+            for (int j = 0; j < data1.length; ++j) {
+                //if (data1[j] != data2[j + sizeof(int)])
+                if (data1[j] != data2[j + 4]) {
+                    ++errCnt;
+                }
+            }
+            if (errCnt > 0) {
+                System.out.print("Errors cnt " + errCnt + " " + data1.length + " !");
+            }
+            byte[] data3 = null;
+            try {
+                data3 = BCHCoder.decode_systematic(data2, t, blockSize);
+            } catch (BchException e) {
+                /*BCHCoder.WriteBits(data1);
+                for (int k = 0; k < data1.length; ++k) {
+                    System.out.println(data1[k]);
+                }
+                System.out.println("");
+                BCHCoder.WriteBits(data2);
+                for (int k = 0; k < data2.length; ++k) {
+                    System.out.println(data2[k]);
+                }
+                System.out.println("");*/
+
+                fail(e.getMessage());
+            }
+
+            assertNotNull(data3);
+            assertEquals(data3.length, data1.length);
+
+            for (int j = 0; j < data3.length; ++j) {
+                assertEquals("Incorrect in byte-to-byte comparing", data3[j], data1[j]);
+            }
+        }
+    }
+}
